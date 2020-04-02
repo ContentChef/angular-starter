bfdc612e1417bf6838096f480c307d1b143d2182
PierpaoloIannone <piannone@byte-code.com>
2020-04-02 09:19:56 +0200

    chore: update contentchef-node to latest version

--- src/app/sites-list/sites-list.component.ts
+++ src/app/sites-list/sites-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ISearchResponse } from '@contentchef/contentchef-node';
+import { IResponse } from '@contentchef/contentchef-node';
 import { ContentChefService } from '../contentChef';
 
 @Component({
@@ -8,7 +8,7 @@ import { ContentChefService } from '../contentChef';
   styleUrls: ['./sites-list.component.css'],
 })
 export class SitesListComponent implements OnInit {
-  sites: ISearchResponse<object>[] = [];
+  sites: IResponse<object>[] = [];
   constructor(private contentChefService: ContentChefService) {}
 
   async ngOnInit() {
